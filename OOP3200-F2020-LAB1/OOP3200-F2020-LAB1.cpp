d2b05d9daac638de9fe8863b39f2593281ee77b4
home <andreagrippa76@gmail.com>
2020-09-20 00:30:17 -0400

    Completed input validation,  storing users input into object attributes and creating multiple Tickets. SetWorkTicket needs to return boolean somehow...

--- OOP3200-F2020-LAB1/OOP3200-F2020-LAB1.cpp
+++ OOP3200-F2020-LAB1/OOP3200-F2020-LAB1.cpp
@@ -46,33 +46,25 @@ public:
 	
 	/* Accessors */
 
+	/*Once called, will return selected attribute of object*/
 	int getWorkTicketNumber() { return workTicketNumber; }
 	int getWorkTicketDateDay() { return day; }
 	int getWorkTicketDateMonth() { return month; }
 	int getWorkTicketDateYear() { return year; }
-	
 	string getClientID() { return workClientID; }
 	string getIssueDescription() { return workClientIssue; }
-	string ShowWorkTicket() const;//returns a string that contains ticket info
+	
+	/*Returns a string that contains ticket info*/
+	string ShowWorkTicket() const;
 	
 
 	/* Mutators */
 	
-	void SetWorkTicket(int ticketNumber, int ticketDay, int ticketMonth, int ticketYear, string id, string description)
-	{
-		SetDate(ticketDay, ticketMonth, ticketYear);
-		SetTicketNumber(ticketNumber);
-		SetId(id);
-		SetIssue(description);
-	}
-	
+	/*Once called, sets all the attributes of the object to the entered input*/
+	void SetWorkTicket(int ticketNumber, int ticketDay, int ticketMonth, int ticketYear, string id, string description);
+
 	/*Once called, sets day,month and year for date*/
-	void SetDate(int dayInput, int monthInput, int yearInput)
-	{
-		day = dayInput;
-		month = monthInput;
-		year = yearInput;
-	}
+	void SetDate(int dayInput, int monthInput, int yearInput);
 	
 	/* Once called, sets ticket number, id and issue description */
 	void SetTicketNumber(int ticketNumberInput) { workTicketNumber = ticketNumberInput; }
@@ -82,64 +74,98 @@ public:
 
 int main()
 {
+	//Input variables for ticket attributes
 	int inputTicketNumber;
 	int inputDay;
 	int inputMonth;
 	int inputYear;
-	
-	const int NUM_TICKETS = 3;
-	
 	string inputClientID;
 	string inputClientIssue;
 	
+	//Constants
+	const int NUM_TICKETS = 3; // total tickets being made
+
+	//Objects
 	WorkTicket ticket [NUM_TICKETS];
 
-	cout << "You will be making " << NUM_TICKETS << " today." << endl;
-	//start for loop?
+	cout << "You will be making " << NUM_TICKETS << " tickets today..." << endl;
+
 	try
 	{
-		cout << "What's your ID:";
-		cin >> inputClientID;
+		/*Input loop, prompts user for ticket attributes using getline() for strings and
+		 *ConsoleInput::ReadInteger() for integers
+		 */
+		for (int i = 0; i < NUM_TICKETS; i++) 
+		{		
+			cout << "Processing Ticket #" << i+1 << endl;
+			
+			cout << "Enter your ticket ID (alpha-numeric):";
+			getline(cin, inputClientID);
+			cout << "\nEnter your ticket number (Whole, positive number):";
+			inputTicketNumber = ConsoleInput::ReadInteger(1);
+			cout << "\nEnter the day the ticket was opened:(min.1 max.31)";
+			inputDay = ConsoleInput::ReadInteger(1,31);
+			cout << "\nEnter the month the ticket was opened:(min.1 max.12)";
+			inputMonth = ConsoleInput::ReadInteger(1, 12);
+			cout << "\nEnter the year the ticket was opened:(min.2000 max.2099)";
+			inputYear = ConsoleInput::ReadInteger(2000, 2099);
+			cout << "\nEnter your work issue description:";
+			getline(cin, inputClientIssue);
 
-		cout << "\nWhat's your ticket number:";
-		inputTicketNumber = ConsoleInput::ReadInteger(1);
-		
+			//once ticket has all valid inputs, place into object array
+			ticket[i].SetWorkTicket(inputTicketNumber, inputDay, inputMonth, inputYear, inputClientID, inputClientIssue);
 
-		cout << "\nWhat day is it today:";
-		inputDay = ConsoleInput::ReadInteger(1, 31);
+			//Ticket confirmation
+			cout << "Ticket #" << i+1 << " processed." << endl;
+			cout << "Press any key to continue...";
+			cin.ignore();
+			system("CLS");
+		}
 
-		cout << "\nWhat month is it:";
-		inputMonth = ConsoleInput::ReadInteger(1, 12);
-
-		cout << "\nWhat year is it:";
-		inputYear = ConsoleInput::ReadInteger(2000, 2099);
-
-		ticket[0].SetWorkTicket(inputTicketNumber, inputDay, inputMonth, inputYear, inputClientID,inputClientIssue);
-
-		cout << "Enter your work description:";
-		cin >> inputClientIssue;
-
-		cout << ticket[0].ShowWorkTicket();
-
+		//Output loop
+		for (int i = 0; i < NUM_TICKETS; i++)
+		{
+			cout << "TICKET ENTRY:" << i+1 << endl;
+			cout << ticket[i].ShowWorkTicket() << endl;
+		}
 	}
+	
+	//Will catch any invalid inputs and give an error
 	catch (exception& ex)
 	{
 		cerr << ex.what() << "Ending the program." << endl;
 	}
 	
-
 }
 
+/*Returns a string that contains ticket info*/
 string WorkTicket::ShowWorkTicket() const
 {
 	stringstream strOutput;
 
-	strOutput << "ID:" << workClientID << "\n" << "TICKET NUMBER:" << workTicketNumber << "\n"
-			  << "DATE:" << day << "/" << month << "/" << year << "\n" << workClientIssue << endl;
+	strOutput << "ID: " << workClientID << "\n" << "TICKET NUMBER: " << workTicketNumber << "\n"
+		<< "DATE: " << day << "/" << month << "/" << year << "\n" << "ISSUE: "<< workClientIssue << endl
+		<< "=====================================================================================";
 
 	return strOutput.str();
 	
 }
- 
+
+/*Once called, sets all the attributes of the object to the entered input*/
+void WorkTicket::SetWorkTicket(int ticketNumber, int ticketDay, int ticketMonth, int ticketYear, string id, string description)
+{
+	SetDate(ticketDay, ticketMonth, ticketYear);
+	SetTicketNumber(ticketNumber);
+	SetId(id);
+	SetIssue(description);
+}
+
+/*Once called, sets day,month and year for date*/
+void WorkTicket::SetDate(int dayInput, int monthInput, int yearInput)
+{
+	day = dayInput;
+	month = monthInput;
+	year = yearInput;
+}
 
 
